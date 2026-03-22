9a99bfc90927418164c93a8a03bd144dd1f97953
Tureen Creative Media <jlflesher@yahoo.com>
2026-03-22 18:47:34 +0000

    0.4.1.2

--- Assets/Reign/Scripts/Input/InputSystem.cs
+++ Assets/Reign/Scripts/Input/InputSystem.cs
@@ -26,8 +26,8 @@ namespace reign
 
             Dictionary_InputKeys.TryGetValue(KEY, out KeyCode[] keys);
 
-            if(keys == null) return false;
-            
+            if (keys == null) return false;
+
             foreach (KeyCode key in keys)
             {
                 bool pressed = TYPE switch
