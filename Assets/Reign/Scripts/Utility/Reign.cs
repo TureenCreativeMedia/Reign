a8f83f985fb23da03fc1f64a8629fb143b5e3d71
Tureen Creative Media <jlflesher@yahoo.com>
2026-03-23 01:27:45 +0000

    0.4.1.3

--- Assets/Reign/Scripts/Utility/Reign.cs
+++ Assets/Reign/Scripts/Utility/Reign.cs
@@ -2,6 +2,6 @@ namespace reign
 {
     public static class Reign
     {
-        public const string string_ReignVersion = "0.4.1.0"; // [0.4.1.0 | MINOR]
+        public const string string_ReignVersion = "0.4.1.3"; // [0.4.1.3 | FEATURE/PATCH]
     }
 }
