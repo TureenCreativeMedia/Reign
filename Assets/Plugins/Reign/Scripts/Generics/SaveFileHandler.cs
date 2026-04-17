4d3125c6c9d11bc47225dd53c9caad6c885f0b2b
Tureen Creative Media <jlflesher@yahoo.com>
2026-04-17 23:19:21 +0100

    (v1.0.0) Save file decryption

--- Assets/Plugins/Reign/Scripts/Generics/SaveFileHandler.cs
+++ Assets/Plugins/Reign/Scripts/Generics/SaveFileHandler.cs
@@ -1,31 +1,38 @@
+using System;
 using System.Collections;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using UnityEditor.Compilation;
+using Reign.Systems;
+using UnityEngine;
 
 namespace Reign.Generics
 {
     public sealed class SaveFileHandler
     {
-        public byte[] Encrypt(string plaintext)
+        byte[] passCache => Encoding.ASCII.GetBytes(GameCertificates.SAVE_PASSWORD);
+        byte[] saltCache => Encoding.UTF8.GetBytes(GameCertificates.SAVE_SALT.ToCharArray());
+
+        byte[] Encrypt(string plaintext)
         {
             return (byte[])EncryptRoutine(plaintext).Current;
         }
 
-        private IEnumerator EncryptRoutine(string plaintext)
+        string Decrypt(byte[] cipher)
+        {
+            return (string)DecryptRoutine(cipher).Current;
+        }
+
+        public IEnumerator EncryptRoutine(string plaintext)
         {
             using Aes aes = Aes.Create();
 
             aes.BlockSize = 128;
             aes.KeySize = 64;
 
-            byte[] password = Encoding.ASCII.GetBytes(GameCertificates.SAVE_PASSWORD);
-            byte[] salt = Encoding.UTF8.GetBytes(GameCertificates.SAVE_SALT.ToCharArray());
-
             using var key = new Rfc2898DeriveBytes(
-                password,
-                salt,
+                passCache,
+                saltCache,
                 GameCertificates.SAVE_ITERATIONS,
                 HashAlgorithmName.SHA256
             );
@@ -43,6 +50,71 @@ namespace Reign.Generics
             }
 
             yield return memStream.ToArray();
+        }
+
+        public IEnumerator DecryptRoutine(byte[] cipher)
+        {
+            using Aes aes = Aes.Create();
+
+            aes.BlockSize = 128;
+            aes.KeySize = 64;
+
+            using var key = new Rfc2898DeriveBytes(
+                passCache,
+                saltCache,
+                GameCertificates.SAVE_ITERATIONS,
+                HashAlgorithmName.SHA256
+            );
+
+            aes.Key = key.GetBytes(32);
+
+            byte[] processedIV = new byte[16];
+
+            using MemoryStream memStream = new(cipher);
+
+            memStream.Read(processedIV, 0, processedIV.Length);
+
+            using CryptoStream cryptoStream = new(memStream, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using StreamReader streamReader = new(cryptoStream);
+
+            yield return streamReader.ReadToEndAsync().ToString();
+        }
+
+        public IEnumerator Load()
+        {
+            var path = Path.Combine(Application.persistentDataPath, GameCertificates.SAVE_FILE_DIRECTORY);
+            GameData gameDataLoaded = null;
+
+            if (!File.Exists(path))
+            {
+                try
+                {
+                    byte[] fileBytes = File.ReadAllBytes(path);
+
+                    string json;
+
+                    if (GameCertificates.SAVE_ENCRYPT)
+                    {
+                        json = Decrypt(fileBytes);
+                    }
+                    else
+                    {
+                        json = Encoding.UTF8.GetString(fileBytes);
+                    }
+
+                    gameDataLoaded = JsonUtility.FromJson<GameData>(json);
+                }
+                catch (Exception e)
+                {
+                    Debug.LogWarning($"Could not load data: {e}");
+                }
+            }
+            else
+            {
+                // Save new data
+            }
+
+            yield return gameDataLoaded;
         }
     }
 }
